d19d79d8fa6076b0bb47582d4e14470fb922a145
Ron <ronjohn4@gmail>
2019-03-27 21:34:45 +0100

    Added find_by helper functions

--- app/userapi/users.py
+++ app/userapi/users.py
@@ -1,40 +1,41 @@
 from flask import jsonify, request, url_for
-from app import db
 from app.models import User
 from app.userapi import bp
 from app.userapi.errors import bad_request
 
-print('User Routes loading...')
 
 @bp.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
-    print('route: GET/users/id')
     return jsonify(User.query.get_or_404(id).to_dict())
+
+
+@bp.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+        user = User.find_by_id(id)
+        if user:
+            user.delete_from_db()
+        return jsonify({'message': 'store deleted'}), 200
 
 
 @bp.route('/users', methods=['POST'])
 def create_user():
-    print('route: POST/users')
     data = request.get_json() or {}
-    if 'username' not in data or 'email' not in data or 'password' not in data:
-        return bad_request('must include username, email and password fields')
+    if 'username' not in data or 'email' not in data:
+        return bad_request('must include username and email')
     if User.query.filter_by(username=data['username']).first():
         return bad_request('please use a different username')
     if User.query.filter_by(email=data['email']).first():
         return bad_request('please use a different email address')
     user = User()
-    user.from_dict(data, new_user=True)
-    db.session.add(user)
-    db.session.commit()
+    user.from_dict(data)
+    user.add_to_db()
     response = jsonify(user.to_dict())
-    response.status_code = 201
     response.headers['Location'] = url_for('userapi.get_user', id=user.id)
-    return response
+    return response, 201
 
 
 @bp.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
-    print('route: PUT/users/id')
     user = User.query.get_or_404(id)
     data = request.get_json() or {}
     if 'username' in data and data['username'] != user.username and \
@@ -43,15 +44,11 @@ def update_user(id):
     if 'email' in data and data['email'] != user.email and \
             User.query.filter_by(email=data['email']).first():
         return bad_request('please use a different email address')
-    user.from_dict(data, new_user=False)
-    db.session.commit()
+    user.from_dict(data)
+    user.update_to_db()
     return jsonify(user.to_dict())
 
 
 @bp.route('/users', methods=['GET'])
 def get_users():
-    print('route: GET/users')
-    page = request.args.get('page', 1, type=int)
-    per_page = min(request.args.get('per_page', 10, type=int), 100)
-    data = User.to_collection_dict(User.query, page, per_page, 'userapi.get_users')
-    return jsonify(data)
+    return jsonify(User.to_collection_dict(User.query.all()))
