294b76e1fa6ef75ecb80f5f6fbba2fbfd0011b41
Ron <ronjohn4@gmail>
2019-03-27 21:32:57 +0100

    Added tests for security

--- tests.py
+++ tests.py
@@ -22,17 +22,38 @@ class UserModelCase(unittest.TestCase):
         self.app_context.pop()
 
     def test_user(self):
-        # create four users
-        u1 = User(username='john', email='john@example.com')
-        u2 = User(username='susan', email='susan@example.com')
+        # create test users
+        u1 = User(username='john', email='john@example.com', active=True, about_me='about me text1')
+        u2 = User(username='susan', email='susan@example.com', active=False, about_me='about me text2')
 
         db.session.add_all([u1, u2])
 
         self.assertEqual(u1, u1)
         self.assertNotEqual(u1, u2)
 
-    def test_user_always_fails(self):
-        self.assertEqual(1, 2)
+    def test_user_saved_attributes(self):
+        # create test user
+        u1 = User(username='john', email='john@example.com', active=True, about_me='about me text1')
+
+        db.session.add(u1)
+
+        u3 = User.find_by_username(u1.username)
+        self.assertEqual(u3.active, u1.active)
+        self.assertEqual(u3.username, u1.username)
+        self.assertEqual(u3.email, u1.email)
+        self.assertEqual(u3.about_me, u1.about_me)
+
+    def test_user_security(self):
+        u1 = User(username='john', email='john@example.com')
+        u1.set_password('Test1Passw0rd!')
+
+        db.session.add(u1)
+
+        self.assertTrue(u1.check_password('Test1Passw0rd!'))
+        self.assertFalse(u1.check_password('test1Passw0rd!'))
+
+    # def test_user_always_fails(self):
+    #     self.assertEqual(1, 2)
 
 
 if __name__ == '__main__':
